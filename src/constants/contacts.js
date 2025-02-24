157eb11889c6869f09b8b6f380bb8fdf3edcab8e
Aargo911 <jekaboy2019@gmail.com>
2025-02-24 16:54:32 +0200

    dd

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -4,8 +4,4 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PATH_DB = path.join(__dirname, '..', 'db', 'db.json');
-
-export default {
-  PATH_DB,
-};
+export const PATH_DB = path.join(__dirname, '..', 'db', 'db.json');
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,14 +1,14 @@
 import { faker } from '@faker-js/faker';
 import readContacts from '../utils/readContacts.js';
 import writeContacts from '../utils/writeContacts.js';
-import createFakeContact from '../utils/createFakeContact.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
 async function generateContacts() {
   const count = parseInt(process.argv[2]) || 5;
   const contacts = await readContacts();
   for (let i = 0; i < count; i++) {
     const newContact = createFakeContact();
-    newContact.name = faker.person.fullName(); 
+    newContact.name = faker.person.fullName();
     contacts.push(newContact);
   }
   await writeContacts(contacts);

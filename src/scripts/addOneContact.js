e13f1af8bfd2e13b597ab3107c172b8922725cde
Aargo911 <jekaboy2019@gmail.com>
2025-02-26 15:03:05 +0200

    d

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,6 +1,6 @@
 import readContacts from '../utils/readContacts.js';
 import writeContacts from '../utils/writeContacts.js';
-import createFakeContact from '../utils/createFakeContact.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
 async function addOneContact() {
   const contacts = await readContacts();
